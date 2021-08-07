e6330d1d8f694ebb8aba6b4af0a01d7f861291bb
Rodrigo Sbampato <rodrigo.sbampato@gmail.com>
2021-08-07 20:29:58 -0300

    cadastro de clientes

---
+++ src/Cadastro_Clientes.jsx
@@ -0,0 +1,138 @@
+import React, {useEffect, useState} from 'react';
+import firebase from '../firebase';
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
+
+export default function AddUsers({navigation}){
+     //constante de verificação que será usada para realizar o retorno a página principal do app. enquanto for false veremos a renderização de confirmação. Quando for true, direciona para o inicio. 
+    const [ok,setOk] = useState(false);
+     //estado que irá receber os valores do firebase para inserção
+    const [state, setState] = useState({
+        nome: '',
+        email: '',
+        endereco:'',
+        numero:'',
+        cep:'',
+        bairro:'',
+        complemento:'',
+        referencia:'',
+        telefone:''
+        
+    });
+
+    // console.log(state);
+    //função para realizar mudança automática no estado
+    const handleInputChange = (name, value) =>{
+        setState({
+            ...state, [name]: value
+        })
+    }
+
+    //inserir em uma collection
+    const addUser = async () => {
+        //através da função assincrona com await ele irá executar o comando de add() onde irá inserir o objeto dentro collection users. Será criado um doc com um id aleatório com as informações passadas dentro dele. A função then irá mostrar uma mensagem de alerta dizendo salved, caso não consiga inserir, será acionado catch.
+        await firebase.db.collection('cadastro').add(state).then(
+            ()=>{
+                alert("salved");
+                //quando ocorre a deleção o estado ok é alterado para true e renderizado
+                setOk(true);
+            }
+        ).catch(
+            ()=>alert("não foi possivel inserir")
+        )
+    }
+    //quando ocorre a renderização por conta da exclusão, por ok ser true ele entra no if e retorna para a página anterior.    
+    if(ok){
+        alert("cadastrado");
+        navigation.popToTop();
+    }
+
+    console.log(state);
+    return(
+        <View style={styles.container}>
+            <Text style={styles.h1}>Cadastro</Text>
+            {/**campo  Nome*/}
+            {/* <Text>{state.nome}</Text>
+            <Text>{state.email}</Text> */}
+            <TextInput
+                style={styles.input}
+                placeholder='Nome'
+                defaultValue={state.nome}
+                onChangeText={
+                    (value)=>handleInputChange('nome', value)
+                }
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='email'
+                textContentType='emailAddress'
+                defaultValue={state.email}
+                onChangeText={
+                    (value)=>handleInputChange('email', value)
+                }
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Endereço'
+                defaultValue={state.endereco}
+                onChangeText={
+                    (value)=>handleInputChange('endereco', value)
+                }
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Numero'
+                defaultValue={state.numero}
+                onChangeText={
+                    (value)=>handleInputChange('numero', value)
+                }
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Cep'
+                defaultValue={state.cep}
+                onChangeText={
+                    (value)=>handleInputChange('cep', value)
+                }
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Bairro'
+                defaultValue={state.bairro}
+                onChangeText={
+                    (value)=>handleInputChange('bairro', value)
+                }
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Complemento'
+                defaultValue={state.complemento}
+                onChangeText={
+                    (value)=>handleInputChange('complemento', value)
+                }
+            />
+             <TextInput
+                style={styles.input}
+                placeholder='Referência'
+                defaultValue={state.referencia}
+                onChangeText={
+                    (value)=>handleInputChange('referencia', value)
+                }
+            />
+            <TextInput
+                style={styles.input}
+                placeholder='Fone'
+                defaultValue={state.telefone}
+                onChangeText={
+                    (value)=>handleInputChange('telefone', value)
+                }
+            />
+
+            <Button
+                style={styles.input}
+                title="Adicionar"
+                onPress={addUser}
+            />
+        </View>
+        
+    )
+}
---
+++ src/Principal2.jsx
@@ -0,0 +1,56 @@
+// import React from 'react';
+// import 'react-native-gesture-handler';
+// import {NavigationContainer} from '@react-navigation/native'
+// import {createDrawerNavigator} from '@react-navigation/drawer';
+// import { StyleSheet, StatusBar, View} from 'react-native';
+// import Icons from 'react-native-vector-icons/MaterialIcons';
+// // COMPONENTES
+// import Login_Clientes from './Login_Clientes';
+// import Home from './Home'
+
+
+// export default function Principal2() {
+//     return(
+//         <Drawer.Navigator initialRouteName={Home}
+//     drawerContentOptions={{
+//       activeTintColor: '#FFF',
+//       inactiveTintColor: '#FFF',
+//       itemStyle: { alignItems:'flex-start',},
+//     }} drawerStyle={{
+//       backgroundColor:  '#2E3192',
+//       alignItems: "center",
+//     }}>
+
+//         <Drawer.Screen name="Login_Clientes" component={Login_Clientes}
+//             options={{
+//             title: 'Login Clientes',
+//             drawerIcon: ({focused, size}) => (
+//                 <Icons
+//                 name="login"
+//                 size={size}
+//                 color={focused ? '#FFF' : '#ED1C24'}
+//                 />
+//             ),
+//             }}
+//         />
+
+
+
+
+
+//     </Drawer.Navigator>
+        
+        
+        
+        
+        
+        
+//     )
+
+
+
+
+
+
+
+// }
