50d95c2df0688116a3e5ee76a8d7e33779c40d1a
Leandro Lesnik <52947547+leandrolesnik@users.noreply.github.com>
2021-08-08 10:35:00 -0300

    Adicionado o footer

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text} from "react-native";
+
+export default function Footer(){
+  return(
+    <View style={{marginTop: 15, backgroundColor: "#ED1C24", padding: 5,}}>
+      <Text style={{color: "#FFF", textAlign: "center"}}>©2021 - Mercantil Cruzeiro Frios</Text>
+    </View>
+  )
+}
