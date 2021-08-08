087cf62441088aa73079e6d74cc5283f26b4d17f
Leandro Lesnik <52947547+leandrolesnik@users.noreply.github.com>
2021-08-08 11:06:23 -0300

    Footer interno

--- src/produtos/molhos.jsx
+++ src/produtos/molhos.jsx
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, Image, Dimensions, FlatList } from 'react-nativ
 import firebase from '../../firebase';
 import ImagedCarouselCard from "react-native-imaged-carousel-card";
 import Header from '../Header';
+import Footer from '../Footer';
 
 export default function Molhos({navigation}) {
   
@@ -30,7 +31,7 @@ export default function Molhos({navigation}) {
   }
 
   return(
-    <View>
+    <View style={{backgroundColor: "#FFF"}}>
       <Header />
       <View style={styles.bgtitulo}>
           <Text style={styles.titulo}>Molhos</Text>
@@ -51,6 +52,7 @@ export default function Molhos({navigation}) {
           </View>
         )}
       />
+      <Footer />
     </View>
   )
 }
