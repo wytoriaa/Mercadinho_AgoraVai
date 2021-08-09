75f99cd48d5f75419187fc74fbc94315250b1530
heloeng <70357454+heloeng@users.noreply.github.com>
2021-08-09 13:17:24 -0300

    alteração estilo botão

--- src/CadastroProdutos.jsx
+++ src/CadastroProdutos.jsx
@@ -1,17 +1,16 @@
 import React, { useState} from 'react';
 import firebase from '../firebase';
-import { StyleSheet, Text, View, TouchableOpacity, Switch, TextInput, ScrollView} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Switch, TextInput} from 'react-native';
 
 export default function CadastroProdutos({navigation}){
+    const[ok, setOk] = useState(false);
    
     const[state, setState] = useState({
         Departamento: '',
         Descrição: '',
         Nome: '',
-        Preço:0,
-        Codigo:0,
+        Preço:'',
         Promoção:false,
-        ValorPromoção:'',
         Quantidade:'',
         urlImg:''
     })
@@ -24,20 +23,11 @@ export default function CadastroProdutos({navigation}){
 
           
     const cadastro = async()=>{
-        await firebase.db.collection("prod").add(state)
+        await firebase.db.collection("produtos").add(state)
         .then(
             ()=>{
                 alert("Produto Cadastrado com Sucesso!");
-                navigation.navigate("ListarProdutos");
-                setState(Departamento, '');
-                setState(Descrição, '');
-                setState(Nome, '');
-                setState(Preço, 0);
-                setState(Codigo, 0);
-                setState(Promoção, false);
-                setState(ValorPromoção, '');
-                setState(Quantidade, '');
-                setState(urlImg, '');
+                setOk(true)
             }    
         )
         .catch(
@@ -50,50 +40,37 @@ export default function CadastroProdutos({navigation}){
 
     return(
         <View style={styles.container}>
-            <ScrollView >
-                <Text style={styles.h1}>Cadastro dos Produtos</Text>
-                <Text>Nome:</Text>
-                <TextInput placeholder="Produto" defaultValue={state.Nome}
-                            onChangeText={(value)=>handleInputChange('Nome', value)} style={styles.input}/>
-                <Text>Departamento:</Text>
-                <TextInput placeholder="Grupo" defaultValue={state.Departamento}
-                            onChangeText={(value)=>handleInputChange('Departamento', value)} style={styles.input}/>
-                <Text>Descrição:</Text>            
-                <TextInput placeholder="Descrição" defaultValue={state.Descrição}
-                            onChangeText={(value)=>handleInputChange('Descrição', value)} style={styles.input}/>
-                <Text>Preço:</Text>
-                <TextInput placeholder="Preço" defaultValue={state.Preço}
-                            onChangeText={(value)=>handleInputChange('Preço', value)} style={styles.input}/>
-                <Text>Codigo:</Text>
-                <TextInput placeholder="Codigo" defaultValue={state.Codigo}
-                            onChangeText={(value)=>handleInputChange('Codigo', value)} style={styles.input}/>
-                <Text>Quantidade:</Text>
-                <TextInput placeholder="Quantidade" defaultValue={state.Quantidade}
-                            onChangeText={(value)=>handleInputChange('Quantidade', value)} style={styles.input}/>              
-                <Text>Promoção:</Text>            
-                <Switch value={state.Promoção} onValueChange={(value)=> setState({...state, Promoção: value})}/>   
+            <Text style={styles.h1}>Cadastro dos Produtos</Text>
+            <TextInput placeholder="Produto" defaultValue={state.Nome}
+                        onChangeText={(value)=>handleInputChange('Nome', value)} style={styles.input}/>
+            <TextInput placeholder="Grupo" defaultValue={state.Departamento}
+                        onChangeText={(value)=>handleInputChange('Departamento', value)} style={styles.input}/>            
+            <TextInput placeholder="Descrição" defaultValue={state.Descrição}
+                        onChangeText={(value)=>handleInputChange('Descrição', value)} style={styles.input}/>
+            <TextInput placeholder="Preço" defaultValue={state.Preço}
+                        onChangeText={(value)=>handleInputChange('Preço', value)} style={styles.input}/>
+            <TextInput placeholder="Quantidade" defaultValue={state.Quantidade}
+                        onChangeText={(value)=>handleInputChange('Quantidade', value)} style={styles.input}/>              
+            <Text>Promoção:</Text>
 
-                <Text>Valor promoção:</Text>
-                <TextInput placeholder="ValorPromoção" defaultValue={state.ValorPromoção}
-                            onChangeText={(value)=>handleInputChange('ValorPromoção', value)} style={styles.input}/>  
-                
-                <Text>urlImg:</Text>
-                <TextInput placeholder="urlImg" defaultValue={state.urlImg}
-                            onChangeText={(value)=>handleInputChange('urlImg', value)} style={styles.input}/>  
-                
-                <TouchableOpacity style={styles.botao} onPress={()=>cadastro()}><Text>Cadastrar</Text></TouchableOpacity>                                
-                
-            </ScrollView>
+           
+
+            
+            <Switch value={state.Promoção} onValueChange={(value)=> setState({...state, Promoção: value})}/>   
+           
+            
+            <TouchableOpacity style={styles.botao} onPress={()=>cadastro()}><Text style={styles.textoBotao}>Cadastrar</Text></TouchableOpacity>                                
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        // flex:1,
+        flex:1,
         backgroundColor: '#fff',
         alignItems: 'center',
-        padding:2
+        padding:12
     }, 
     h1:{
         fontSize:20,
@@ -109,13 +86,23 @@ const styles = StyleSheet.create({
         borderRadius:10,
         borderColor:'grey'
     },
+
     botao:{
         backgroundColor:'#3bbdc2',
         margin:10,
         padding: 10,
-        alignItems: 'center',
-        borderRadius: 10
-    }
+        borderRadius: 10,
+    },
 
     
+textoBotao:{
+
+    color: "white",
+    
+    },
+
+
+
+
+
 })
--- src/DeleteProdutos.jsx
+++ src/DeleteProdutos.jsx
@@ -1,71 +1,129 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import firebase from '../firebase';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import ListarProdutos from './ListarProdutos';
 
-export default function DeleteProdutos({route, navigation}){
-   
-    const [ok,setOk] = useState(false);
+export default function DeleteProdutos({ route, navigation }) {
 
-    const key = route.params;
-    
-    
-    const deleteProdutos = async () =>{
-   
-    const produtos = firebase.db.collection('prod');
-    
+  const [ok, setOk] = useState(false);
+
+  const key = route.params;
+
+
+  const deleteProdutos = async () => {
+
+    const produtos = firebase.db.collection('produtos');
+
     await produtos.doc(key).delete()
-    .then(
-      ()=>{
+      .then(
+        () => {
           alert("deleted");
-        
+
           setOk(true);
-    }
-    ).catch(
-      ()=>alert("não encontrado")
-    )
+        }
+      ).catch(
+        () => alert("não encontrado")
+      )
   }
-   
-    if(ok){
-      
-        navigation.popToTop();
-    }
+
+  if (ok) {
+
+    navigation.popToTop();
+  }
 
 
-   return(
-        <View style={styles.container}>
-            <Text style={styles.h1}>Deseja excluir o Produto?</Text>
-            {/**o botão abaixo aciona a função de exclusão */}
-            <Button
-                title="Sim"
-                onPress={deleteProdutos}
-            />
-            {/**o botão abaixo aciona a função de remover todos da pilha */}
-            <Button
-                title="Não"
-                onPress={()=>navigation.popToTop()}
-            />
-        </View>
-        
-    )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.h1}>Deseja excluir o Produto?</Text>
+      {/**o botão abaixo aciona a função de exclusão */}
+      <View style={styles.areaBotao}>
+        {/* <Button
+          title="Sim"
+          onPress={deleteProdutos}
+        /> */}
+<TouchableOpacity style={styles.botaoDeletar} onPress={deleteProdutos}><Text style={styles.textoBotao}>Sim</Text></TouchableOpacity>
+
+        {/**o botão abaixo aciona a função de remover todos da pilha */}
+        {/* <Button
+          title="Não"
+           onPress={() => navigation.popToTop()}
+        /> */}
+<TouchableOpacity style={styles.botao}  onPress={() => navigation.popToTop()}><Text style={styles.textoBotao}>Não</Text></TouchableOpacity>
+
+
+      </View>
+
+
+    </View>
+
+  )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems:'center',
-    padding:12
+    alignItems: 'center',
+    padding: 12
   },
-  h1:{
-      fontSize:20,
-      fontWeight:'bold',
-      padding:12
-  }, 
-  input:{
-    height:60, 
-    width:'90%',
-    borderWidth:1, 
-    padding:10,
-    marginTop:5
-  }
+  h1: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 12
+  },
+  input: {
+    height: 60,
+    width: '90%',
+    borderWidth: 1,
+    padding: 10,
+    marginTop: 5,
+  },
+
+
+
+   areaBotao: {
+   height: 20,
+    flex: 1,
+   flexDirection: "row",
+  
+    
+   },
+
+
+
+   botaoDeletar:{
+    backgroundColor:'red',
+    margin:10,
+    padding: 10,
+    borderRadius: 10,
+    height: 40,
+    width: 60,
+},
+
+
+
+textoBotao:{
+textAlign: "center",
+  color: "white",
+  
+  },
+
+
+
+
+  botao:{
+    backgroundColor:'#3bbdc2',
+    margin:10,
+    padding: 10,
+    borderRadius: 10,
+    height: 40,
+    width: 60,
+},
+
+
+
+
+
+
+
 });
+
